c0a4188dede4242171718d32cebd1f7f9fc6c863
admin <ncstech07@gmail.com>
2015-10-27 16:40:35 -0400

    updated Qt Resource files

--- D1/mainwindow.cpp
+++ D1/mainwindow.cpp
@@ -5,10 +5,24 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    QPalette palette;
+    palette.setBrush(this->backgroundRole(), QBrush(QImage(":Images/loginScreenIMG")));
+
+    this->setPalette(palette);
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_buttonSignIn_clicked()
+{
+
+}
+
+void MainWindow::on_buttonSignUp_clicked()
+{
+
 }
