79f873655943045e01a4f86ac4311c6b6a5bf4ef
derek <derek.felson@carleton.ca>
2015-10-31 14:10:21 -0400

    Fixed compile error from old merge

--- D1/adminhomeview.cpp
+++ D1/adminhomeview.cpp
@@ -35,18 +35,3 @@ void AdminHomeView::on_runAlgoBtn_clicked()
 {
     _control.computeTeams(-1); // TODO: actually pass in the selected id
 }
-<<<<<<< HEAD
-=======
-
-/*void AdminHomeView::on_listOfProjectLv_doubleClicked(const QModelIndex &index)
->>>>>>> f8e1e5003a143e6fca704cc0a5b84b1bc749a24d
-{
-
-}
-
-void AdminHomeView::on_listOfStudentsLv_doubleClicked(const QModelIndex &index)
-{
-
-
-}*/
->>>>>>> 50f83bcf7cab18d7e9afd81227cfcf5b1e524778
