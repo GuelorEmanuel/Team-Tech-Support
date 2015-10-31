6f31412b523fc1182c7711f63a8aa62de86838f9
Coldmusic <margaret.otochkina@gmail.com>
2015-10-31 03:07:45 -0400

    PLEASE UPDATE DB PATH FOR YOU COMPUTER(mainWindowControl.cpp)!

--- D1/mainWindowControl.cpp
+++ D1/mainWindowControl.cpp
@@ -13,7 +13,25 @@ MainWindowControl::~MainWindowControl()
     // Close database
 }
 
-void MainWindowControl::signIn(QString userName)
+//Function: opens Database and returns status to user:
+//0 - wasn't able to open
+//1- Success
+//Huge note!!! db location - change it if it differs on you computer!!! Much Depends!1
+int MainWindowControl::openDB()
+{
+    _db = QSqlDatabase::addDatabase("QSQLITE");
+    _db.setDatabaseName("/home/admin/cupid/new_branch/Team-Tech-Support/D1/mydb.sqlite");
+
+    if(!_db.open()) {
+        qDebug() << "Was not able to open DB.";
+        return 0;
+    } else {
+        qDebug() << "Success!";
+        return 1;
+    }
+}
+
+int MainWindowControl::signIn(QString userName)
 {
     // Select username from database
     // If student, create and launch student control
@@ -21,6 +39,40 @@ void MainWindowControl::signIn(QString userName)
     // Otherwise display user not found error
 
     // Need to check with the controller first if the model was able to connect to the database
+    int stat = openDB();
+    if(stat == 0) return -1;
+
+    //_view.hide();
+
+    QSqlQuery qry(_db);
+    qDebug() << userName;
+    qry.prepare("SELECT * FROM user WHERE username = '"+userName+"'");
+    QString id = "";
+    if(!qry.exec()){
+        qDebug() << qry.lastError();
+        return -1;
+    } else {
+        int count = 0;
+        while(qry.next()){
+            ++count;
+            id = qry.value(3).toString();
+        }
+        if(count == 1) {
+            qDebug() << "Login is valid";
+            if(id == "0") {
+                //create admin view
+                qDebug() << "We caught admin!";
+            } else {
+                //create student view
+            }
+            return 1;
+        } else {
+            qDebug() << "Invalid login";
+            _view.show();
+            return 0;
+        }
+    }
+    return 0;
     //mainWindowControl->on_buttonSignIn_clicked();
     //_view.hide();
 }
