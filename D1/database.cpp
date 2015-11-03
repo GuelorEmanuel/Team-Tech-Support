dc0247466250ff2ed3126673b79dc2c8fa7364be
Coldmusic <margaret.otochkina@gmail.com>
2015-11-03 13:04:44 -0500

    close project when created

--- D1/database.cpp
+++ D1/database.cpp
@@ -2,7 +2,7 @@
 
 Database::Database() {
     _db = QSqlDatabase::addDatabase("QSQLITE");
-    _db.setDatabaseName("/home/admin/Desktop/Team-Tech-Support/D1/mydb.sqlite");
+    _db.setDatabaseName("./mydb.sqlite");
     _db.open(); // ignore return code
 }
 
