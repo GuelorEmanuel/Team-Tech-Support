47d5d4750a87b7c822d500ce93968a9ef264daa9
admin <ncstech07@gmail.com>
2015-12-04 00:39:30 -0500

    rolled back branch

--- cupid/Storage/storagemanager.cpp
+++ cupid/Storage/storagemanager.cpp
@@ -3,3 +3,28 @@
 StorageManager::StorageManager()
 {
 }
+StorageManager::~StorageManager()
+{
+}
+ bool StorageManager::createAdmin(const Admin &){
+
+ }
+ Student &StorageManager::getStudent() {
+
+ }
+ void StorageManager::createAdmin(const Admin &admin) {
+
+ }
+ Admin& StorageManager::getAdmin(int id){
+
+ }
+ User& StorageManager::getUser(int id){
+
+ }
+
+ bool StorageManager::userExists(QString name){
+
+ }
+ bool StorageManager::editProfile(const Profile &profile){
+
+ }
